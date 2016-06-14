3cb16ffd3f95bd397d4d628c089eccbc2584ce8c
kumavis <aaron@kumavis.me>
2016-06-14 14:17:43 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const lookupOpcode = require('ethereumjs-vm/lib/opcodes.js')
+
+
+
+module.exports = binToOps
+
+function binToOps(contractCode){
+  var ops = []
+  for (var index = 0; index < contractCode.length; index++) {
+    var currentOp = lookupOpcode(contractCode[index], true)
+    ops.push(currentOp)
+    // handle PUSH inline data
+    if (currentOp.name.slice(0, 4) === 'PUSH') {
+      // load inline data
+      var pushDataLength = contractCode[index] - 0x5f
+      var pushData = contractCode.slice(index + 1, index + pushDataLength + 1)
+      currentOp.pushData = pushData
+      // skip read of inline data
+      index += pushDataLength
+    }
+  }
+  return ops
+}
